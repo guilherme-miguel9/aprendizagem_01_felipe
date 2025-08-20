a9099ddf343a765f39b3bd3ad5b3cf9af711ad54
Guilherme <guicaruaru@hotmail.com>
2025-08-20 00:21:34 +0000

    add more information in response.status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
+import { Connection } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sun;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const postVers = await database.query('SELECT VERSION();');
+  const maxConnect = await database.query('SHOW max_connections;');
+  const peopleActivity = await database.query('SELECT count(*) FROM pg_stat_activity;');
+  const bdVers = postVers.rows[0].version;
+  const bdMax = maxConnect.rows[0].max_connections;
+  const bdActivity = peopleActivity.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    bd_version: bdVers,
+    max_connect: bdMax,
+    bd_activity: bdActivity,
+  });
+
+  
 }
 
 export default status;
