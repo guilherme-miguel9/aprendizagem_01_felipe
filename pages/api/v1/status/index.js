cf0ff01aefe907546e744594abdec6d169450db6
Guilherme <guicaruaru@hotmail.com>
2025-09-02 23:18:10 +0000

    finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,7 +5,12 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const postVers = await database.query('SHOW server_version;');
   const maxConnect = await database.query('SHOW max_connections;');
-  const peopleActivity = await database.query('SELECT count(*) FROM pg_stat_activity;');
+  const databaseName = process.env.POSTGRES_DB;
+  const peopleActivity = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  
 
   const bdVers = postVers.rows[0].server_version;
   const bdMax = maxConnect.rows[0].max_connections;
@@ -16,7 +21,8 @@ async function status(request, response) {
     dependencies: {
       database: {
         version: bdVers,
-        max_connection: bdMax,
+        max_connection: parseInt(bdMax),
+        activity: bdActivity,
       },
     },
   });
